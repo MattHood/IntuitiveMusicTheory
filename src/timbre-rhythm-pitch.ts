61431d9ce02fd3a531f9f014c9f2edbb2dbc596f
Matt Hood <matthood0@gmail.com>
2021-02-14 14:54:15 +1100

    Slight refactor to timbre rhythm pitch

--- src/timbre-rhythm-pitch.ts
+++ src/timbre-rhythm-pitch.ts
@@ -11,6 +11,7 @@ const chords: any = [{note: Fm7, duration: "4n", time: "0", velocity: 0.6},
 		     {note: Fm7, duration: "8n", time: "0:3", velocity: 0.6},
 		     {note: Bbm7, duration: "4n", time: "1:0", velocity: 0.6},
 		     {note: Abadd9, duration: "8n", time: "1:3", velocity: 0.6}];
+
 const kick: any = [{note: "F1", duration: "16n", time: "0"},
 		   {note: "F1", duration: "16n", time: "0:0:3"},
 		   {note: "C3", duration: "16n", time: "0:1:0"},
@@ -28,20 +29,54 @@ const kick: any = [{note: "F1", duration: "16n", time: "0"},
 		  ];
 
 
-		     
 
-export default class TimbreRhythmPitch {
-  melodySynth: Tone.Synth;
-  bassSynth: Tone.FMSynth;
-  chordSynth: Tone.PolySynth;
-  kickSynth: Tone.MembraneSynth;
+class FX {
   verb: Tone.Reverb;
   filter: Tone.Filter;
   distortion: Tone.Distortion;
   filterGain: Tone.Gain;
   distortionGain: Tone.Gain;
   dryGain: Tone.Gain;
-  fx: Tone.Gain;
+  entry: Tone.Gain;
+  exit: Tone.Gain;
+
+  constructor() {
+    this.exit = new Tone.Gain(1);
+    this.verb = new Tone.Reverb().connect(this.exit);
+    this.verb.wet.value = 0.5;
+
+    this.filterGain = new Tone.Gain(0).connect(this.verb);
+    this.distortionGain = new Tone.Gain(0).connect(this.verb);
+    this.dryGain = new Tone.Gain(1).connect(this.verb);
+    this.filter = new Tone.Filter(200, "lowpass", -24).connect(this.filterGain);
+    this.distortion = new Tone.Distortion(0.2).connect(this.distortionGain);
+    
+    this.entry = new Tone.Gain().connect(this.filter);
+    this.entry.connect(this.distortion);
+    this.entry.connect(this.dryGain);
+  }
+
+  set filterAmount(f: number) {
+    this.filterGain.gain.rampTo(f, 0.1);
+  }
+
+  set distortionAmount(d: number) {
+    this.distortionGain.gain.rampTo(d, 0.1);
+  }
+
+  set dryAmount(d: number) {
+    this.dryGain.gain.rampTo(d, 0.1);
+  }
+
+}
+		     
+
+export default class TimbreRhythmPitch {
+  fx: FX;
+  melodySynth: Tone.Synth;
+  bassSynth: Tone.FMSynth;
+  chordSynth: Tone.PolySynth;
+  kickSynth: Tone.MembraneSynth;
 
   melodyPart: Tone.Part;
   bassPart: Tone.Part;
@@ -50,91 +85,67 @@ export default class TimbreRhythmPitch {
 
   pitchShift: number;
   
-
-
   constructor() {
-    this.verb = new Tone.Reverb().toDestination();
-    this.verb.wet.value = 0.5;
 
-    this.filterGain = new Tone.Gain(0).connect(this.verb);
-    this.distortionGain = new Tone.Gain(0).connect(this.verb);
-    this.dryGain = new Tone.Gain(1).connect(this.verb);
-    this.filter = new Tone.Filter(200, "lowpass", -24).connect(this.filterGain);
-    this.distortion = new Tone.Distortion(0.2).connect(this.distortionGain);
-    this.fx = new Tone.Gain().connect(this.filter);
-    this.fx.connect(this.distortion);
-    this.fx.connect(this.dryGain);
+
+    this.fx = new FX();
+    
+    this.melodySynth = new Tone.Synth().connect(new Tone.Panner(0.8))
+      .connect(this.fx.entry);
+    this.bassSynth = new Tone.FMSynth()
+      .connect(this.fx.entry);
+    this.chordSynth = new Tone.PolySynth().connect(new Tone.Panner(-0.8))
+      .connect(this.fx.entry);
+    this.kickSynth = new Tone.MembraneSynth()
+      .connect(this.fx.entry);
+
+    this.fx.exit.toDestination();
 
     this.timbre = 64;
-    this.pitchShift = 0;
-    
-    this.melodySynth = new Tone.Synth()
-      .connect(new Tone.Panner(0.8))
-      .connect(this.fx);
-    this.bassSynth = new Tone.FMSynth()
-      .connect(this.fx);
-    this.chordSynth = new Tone.PolySynth()
-      .connect(new Tone.Panner(-0.8))
-      .connect(this.fx);
-    this.kickSynth = new Tone.MembraneSynth()
-      .connect(this.fx);
+    this.pitch = 64;
+    this.rhythm = 64;
 
+    const melodyPlayer = (time, value): void => {
+      let note: string = Tone.Frequency(value.note).transpose(this.pitchShift).toNote();
+	    this.melodySynth.triggerAttackRelease(note, value.duration, time, value.velocity);
+    }
 
-    function playFunc(synth: any, ref: TimbreRhythmPitch): (time, value) => void {
-      return function(time, value) {
-	      if(Array.isArray(value.note)) {
-	        let notes: string[] =
-            value.note
-	      .map(
-		(el) => Tone.Frequency(el)
-		  .transpose(ref.pitchShift)
-		  .toNote()
-	      );
-	  synth.triggerAttackRelease(notes, value.duration, time, value.velocity);
-	}
-	else {
-	  let note: string =
-	    Tone.Frequency(value.note)
-	      .transpose(ref.pitchShift)
-	      .toNote();
-	  synth.triggerAttackRelease(note, value.duration, time, value.velocity);
-	}
-      };
+    const bassPlayer = (time, value): void => {
+      let note: string = Tone.Frequency(value.note).transpose(this.pitchShift).toNote();
+	    this.bassSynth.triggerAttackRelease(note, value.duration, time, value.velocity);
+    }
+
+    // Handles polyphony
+    const chordPlayer = (time, value): void => {
+      let notes: string[] = value.note.map(
+        (el) => Tone.Frequency(el).transpose(this.pitchShift).toNote());
+	    this.chordSynth.triggerAttackRelease(notes, value.duration, time, value.velocity);
+    }
+
+    // Invariant under transposition
+    const kickPlayer = (time, value): void => {
+      this.kickSynth.triggerAttackRelease(value.note, value.duration, time, value.velocity);
     }
     
-    this.melodyPart = new Tone.Part(playFunc(this.melodySynth, this), stringToMusic(melody)).start(0);
-    this.bassPart = new Tone.Part(playFunc(this.bassSynth, this), stringToMusic(bassline)).start(0);  
-    this.chordPart = new Tone.Part(playFunc(this.chordSynth, this), chords).start(0);
-    this.kickPart = new Tone.Part(playFunc(this.kickSynth, this), kick).start(0);
+    this.melodyPart = new Tone.Part(melodyPlayer, stringToMusic(melody)).start(0);
+    this.bassPart = new Tone.Part(bassPlayer, stringToMusic(bassline)).start(0);  
+    this.chordPart = new Tone.Part(chordPlayer, chords).start(0);
+    this.kickPart = new Tone.Part(kickPlayer, kick).start(0);
     
     
   }
   
   set timbre(t: number) {
-    //this.timbre = t;
-    
     const scale = (num: number): number =>  num / 64;
-    let freqComponent = t > 64 ? 0 : scale(64 - t);
-    let distComponent = t < 64 ? 0 : scale(t - 64);
-    let dryComponent  = t < 64 ? scale(t) : scale(128 - t);
-
-    this.filterGain.gain.rampTo(freqComponent, 0.1);
-    this.distortionGain.gain.rampTo(distComponent, 0.1);
-    this.dryGain.gain.rampTo(dryComponent, 0.1);
+    this.fx.filterAmount = t > 64 ? 0 : scale(64 - t);
+    this.fx.distortionAmount = t < 64 ? 0 : scale(t - 64);
+    this.fx.dryAmount  = t < 64 ? scale(t) : scale(128 - t);
   }
-
-  // get timbre(): number {
-  //   return this.timbre;
-  // }
 
   set rhythm(r: number) {
     const scale = (num: number): number =>  (num / 127) * 80 + 40;
     Tone.Transport.bpm.rampTo(scale(r));
   }
-
-  // get rhythm(): number {
-  //   return this.rhythm;
-  // }
 
   set pitch(p: number) {
     // Take pitch from [0, 128) -> [-24, 24), two octaves
@@ -142,14 +153,7 @@ export default class TimbreRhythmPitch {
     this.pitchShift = pitchScale(p);
   }
 
-  get pitch(): number {
-    return this.pitchShift;
-  }
-
-  
-
   play(): void {
-    this.rhythm = 64;
     Tone.Transport.start();
     Tone.Transport.loop = true;
     Tone.Transport.setLoopPoints(0, "2:0");
@@ -160,3 +164,5 @@ export default class TimbreRhythmPitch {
   }
 
 }
+
+
